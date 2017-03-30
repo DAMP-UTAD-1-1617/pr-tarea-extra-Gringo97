2ba5caeaca323be3f4a315493e70b5154388b9f7
oscar.fuente <oscar.fuente@WKSALMPBB443.u-tad.es>
2017-03-30 17:07:35 +0200

    Commit Final

--- PR - Tarea Extra/src/EJERCICIO2/Ejercicio2.java
+++ PR - Tarea Extra/src/EJERCICIO2/Ejercicio2.java
@@ -1,32 +1,23 @@
 package EJERCICIO2;
 
+import java.util.Arrays;
+
 public class Ejercicio2 {
-	/**
-	 * Debes imprimir todos los números de un array, excepto el mayor y el menor
-	 */
+
 	public static void main(String[] args) {
 		int[] ejemplo1 = { 1, 2, 3, 4, 5, 6, 7 }; // Mostraría 2 3 4 5 6
 		int[] ejemplo2 = { 8, 4, 2, 4, 3, 2 }; // Mostraría 4 4 3
-		int min = ejemplo1[0];
-		int max = ejemplo1[0];
-		for (int i = 0; i < ejemplo1.length; i++) {
-			if (ejemplo1[i] >= max) {
-				max = ejemplo1[i];
-			} else if(ejemplo1[i] <= min) {
-				min = ejemplo1[i];
-			}
-		
-		if(ejemplo1[i]< max || ejemplo1[i] > min) {
-				System.out.print(ejemplo1[i]);
-			}
-			
-	
-		
-		
-			
-		
-		
+		mostrarArray(ejemplo1);
+		mostrarArray(ejemplo2);
+
+	}
+
+	private static void mostrarArray(int[] a) {
+		Arrays.sort(a);
+		for (int i = 0; i < a.length; i++) {
+			if (a[i] != a[0] && a[i] != a[a.length - 1])
+				System.out.print(a[i]);
+		}
+		System.out.println();
 	}
 }
-}
-
--- PR - Tarea Extra/src/EJERCICIO4/Ejercicio4.java
+++ PR - Tarea Extra/src/EJERCICIO4/Ejercicio4.java
@@ -1,19 +1,40 @@
 package EJERCICIO4;
 
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Ejercicio4 {
-	static java.util.Scanner in;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int x = 1;
+		while (x != 0) {
+			x = sc.nextInt();
+			if (x != 0) {
+				HashMap<Integer, Integer> valores = new HashMap<Integer, Integer>();
+				for (int i = 0; i < x; i++) {
+					int a = sc.nextInt();
+					int res = a % 10;
+					if (valores.containsKey(res)) {
+						valores.put(res, valores.get(res) + 1);
+					} else {
+						valores.put(res, 1);
+					}
+				}
+				String salida = "";
+				int cant = 0;
+				for (Map.Entry<Integer, Integer> entry : valores.entrySet()) {
+					if (entry.getValue() > cant) {
+						cant = entry.getValue();
+						salida = "" + entry.getKey();
+					} else if (entry.getValue() == cant) {
+						salida = "VARIAS";
+					}
+				}
+				System.out.println(salida);
 
-	public static boolean casoDePrueba() {
-		int n = Integer.parseInt(in.nextLine());
-		if (n == 0)
-			return false;
-		// Programa aquí
-		return true;
-	}
-
-	public static void main(String args[]) {
-		in = new java.util.Scanner(System.in);
-		while (casoDePrueba())
-			;
+			}
+		}
 	}
 }
